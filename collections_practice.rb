a2e405ac03a51327bdfc169a2741e19d51a15d18
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-11 16:45:08 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,6 +7,7 @@ def sort_array_desc(integers)
 end
 
 def sort_array_char_count(strings)
-  strings.sort {|x,y| +(x <=> y)}
+  strings.sort {|left, right|
+    left.length <=> right.length
+  end
 end
-  
