af987fb2ae2c1aebabb98f2629fc078597d44dd9
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-11 16:36:55 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,8 +2,14 @@ def sort_array_asc(integers)
   integers.sort {|x,y| +(x <=> y)}
 end
 
-
 def sort_array_desc(integers)
   integers.sort {|x,y| -(x <=> y)}
 end
 
+def sort_array_char_count(strings)
+  copy = []
+  copy << strings.sort {|{|x,y| -(x <=> y)}
+  end
+  copy
+end
+  
